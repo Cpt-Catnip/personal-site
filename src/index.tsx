d17c48fe600af279d72d54809c63d71f171bd9ed
Mike Koshakow <mzkosh@gmail.com>
2020-09-15 23:41:11 -0400

    I shouldn't do this

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import { render } from 'react-dom';
 type AppProps = { message: string }; /* could also use interface */
 const App = ({ message }: AppProps) => <div>{message}</div>;
 
-render(<App message="Michael Koshakow"></App>, document.getElementById('root'));
+render(<App message="Michael Koshakow" />, document.getElementById('root'));
