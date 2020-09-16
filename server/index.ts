a7453bf2832dd9aa9c5b0560251121d87980c58e
Mike Koshakow <mzkosh@gmail.com>
2020-09-16 17:30:36 -0400

    add server/index.js

---
+++ server/index.ts
@@ -0,0 +1,7 @@
+// module imports
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+// constants
+const PORT: number = process.env.PORT || 8080;
