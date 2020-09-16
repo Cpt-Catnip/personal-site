9218d9719b02654fba58eaa1745f2d891e7058ee
Mike Koshakow <mzkosh@gmail.com>
2020-09-15 23:28:54 -0400

    hello world

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render<Element>(element: <h1>Michael Z Koshakow</h1>, container: )
+render(<h1>Michael Zev Koshakow</h1>, document.getElementById('root'));
