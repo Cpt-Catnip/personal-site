c26691c6e30caf5c320d0f0b7ba4f31d9724f8a3
Mike Koshakow <mzkosh@gmail.com>
2020-09-18 10:23:55 -0400

    remove types from server.js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 // module imports
 const express = require('express');
-import { NextFunction } from 'express';
 const morgan = require('morgan');
 const path = require('path');
-const HttpError = require('./HttpError');
 
 // constants
-const PORT: number | string = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 // create app
 const app = express();
@@ -25,23 +23,16 @@ app.use(express.static(path.join(__dirname, '..', 'dist')));
 // api routes
 
 // serve index.html
-app.use('*', (req: Express.Request, res: any) => {
+app.use('*', (reqRequest, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist/index.html'));
 });
 
 // error handling middleware
-app.use(
-  (
-    err: typeof HttpError,
-    req: Express.Request,
-    res: any,
-    next: NextFunction
-  ) => {
-    console.error(err);
-    console.error(err.stack);
-    res.status(err.status || 500).send(err.message || 'Inernal server error.');
-  }
-);
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Inernal server error.');
+});
 
 // listen
 const server = app.listen(PORT, () => {
