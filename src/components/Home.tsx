059fa019261a8a2138597d70298961a13bc807a6
Mike Koshakow <mzkosh@gmail.com>
2021-04-27 19:50:47 -0400

    escapes apostrophes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,10 +7,10 @@ const Home = () => {
       <h1>hey :)</h1>
       <p>Thanks for checking out my website.</p>
       <p>
-        I'm Mike. I'm a full-stack software engineer and ceramic artist. I'm
-        still working on this website but pretty soon it's going to be full of
-        great content like overviews of the various projects I've worked on and
-        a gallery of my ceramics.
+        I&apos;m Mike. I&apos;m a full-stack software engineer and ceramic
+        artist. I&apos;m still working on this website but pretty soon it&apos;s
+        going to be full of great content like overviews of the various projects
+        I&apos;ve worked on and a gallery of my ceramics.
       </p>
       <p>
         In the mean time, feel free to check me out at any of these other
